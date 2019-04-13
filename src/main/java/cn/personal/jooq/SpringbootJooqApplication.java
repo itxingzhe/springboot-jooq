a9020c972264d701b536902d0e54ce684ca72afb
wangyibin <wangyibin0622@126.com>
2019-04-13 10:30:50 +0800

    Initial commit

---
+++ src/main/java/cn/personal/jooq/SpringbootJooqApplication.java
@@ -0,0 +1,13 @@
+package cn.personal.jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJooqApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootJooqApplication.class, args);
+  }
+
+}
